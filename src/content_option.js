1a7e676fbf3142a6ef31167adf091792d7baf7cc
Arbaz Khan <marbazkhaan07@gmail.com>
2024-12-02 18:11:15 +0500

    First Project
    Date:2/12/2024

---
+++ src/content_option.js
@@ -0,0 +1,158 @@
+const logotext = "Arbaz";
+const meta = {
+    title: "Arbaz Khan",
+    description: "I’m Arbaz Khan data scientist _ Full stack devloper,currently working in Peshawer",
+};
+
+const introdata = {
+    title: "I’m Arbaz Khan",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I am a Student at Fast University",
+    },
+    description: "I am Arbaz Khan, an AI enthusiast and BS AI student at FAST University, Peshawar. Passionate about coding and innovation, I am committed to leveraging artificial intelligence to solve complex problems. Explore my projects and achievements to see my dedication and expertise in the field.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my Self",
+    aboutme: "I am Arbaz Khan, an AI enthusiast and BS AI student at FAST University, Peshawar. Passionate about coding and innovation, I am committed to leveraging artificial intelligence to solve complex problems. Explore my projects and achievements to see my dedication and expertise in the field.",
+};
+const worktimeline = [{
+        jobtitle: "BS AI",
+        where: "Fast University Peshawer",
+        date: "2024",
+    },
+    {
+        jobtitle: "F:SC",
+        where: "Peace Collage Charsadda",
+        date: "2023",
+    },
+    {
+        jobtitle: "School",
+        where: "New Islamia Public High School",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I am specialize in creating intuitive and visually appealing user interfaces and experiences. My UI/UX design work focuses on enhancing user satisfaction by improving the usability and accessibility of digital products",
+    },
+    {
+        title: "Wordpress Design",
+        description: "I have experience in designing and customizing WordPress websites, offering clients dynamic, responsive, and aesthetically pleasing web solutions that meet their unique needs.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "marbazkhaan07@gmail.com",
+    YOUR_FONE: "03339161956",
+    description: "Msg me on any of the above platforms or directly on my email in the below space",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
